d69a8d362d389d95ebac73fea7376e043692de9d
kevinnail <fumalicious@gmail.com>
2022-09-20 11:42:45 -0700

    add display message function and DOM object for it

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ import { getRandomItem } from './utils.js';
 /* Get DOM Elements */
 const userHP = document.getElementById('user-HP');
 const userImg = document.getElementById('robot-me');
-
+const currentMsg = document.getElementById('current-msg');
 /* State */
 let playerObj = {
     HP: 10,
 };
+let message = '';
 /* Events */
 
 /* Display Functions */
@@ -19,8 +20,14 @@ function displayUserHP() {
         userImg.src = '/assets/robots/robot-me.png';
     } else {
         userImg.src = '/assets/other/dead-hero.png';
+        message = 'You died! You have lost to the evil animal robots!';
     }
+}
+
+function displayCurrentMsg() {
+    currentMsg.innerHTML = message;
 }
 // (don't forget to call any display functions you want to run on page load!)
 
 displayUserHP();
+displayCurrentMsg();
