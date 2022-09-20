cad69f6aa5fb397cad740347ab6e8b8d5db33084
kevinnail <fumalicious@gmail.com>
2022-09-20 11:30:18 -0700

    add display alive/ dead player with HP

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 /* Imports */
+import { getRandomItem } from './utils.js';
 
 /* Get DOM Elements */
+const userHP = document.getElementById('user-HP');
+const userImg = document.getElementById('robot-me');
 
 /* State */
-
+let playerObj = {
+    HP: 10,
+};
 /* Events */
 
 /* Display Functions */
+function displayUserHP() {
+    userHP.textContent = playerObj.HP;
 
+    if (!playerObj.HP < 1) {
+        userImg.src = '/assets/robots/robot-me.png';
+    } else {
+        userImg.src = '/assets/other/dead-hero.png';
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
+
+displayUserHP();
